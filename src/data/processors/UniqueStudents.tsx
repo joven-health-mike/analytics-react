d87edc27eef682d944786c27eec7b46bd89351b0
Mike Burke <mike@jovenhealth.com>
2024-04-28 21:48:54 -0600

    Fix issue with uniqueStudents calculation

--- src/data/processors/UniqueStudents.tsx
+++ src/data/processors/UniqueStudents.tsx
@@ -8,10 +8,13 @@ class UniqueStudents implements ISessionProcessor {
 
   processNewSession(session: Session) {
     if (session.isDirect()) {
-      for (const studentName of session.sessionStudents) {
+      const students = session.sessionStudents
+        .split(",")
+        .map((student) => student.trim())
+      students.forEach((studentName) => {
         const newUniqueStudents = this.uniqueStudents.get(studentName) ?? 0
         this.uniqueStudents.set(studentName, newUniqueStudents + 1)
-      }
+      })
     }
   }
 
